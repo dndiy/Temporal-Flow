aa627b25d2f4d52ce07e39c003f47181bf635b6d
Greg Aster <greg@dndiy.org>
2025-02-27 12:07:28 -0800

    Banner responsive layout changes

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,5 @@
+import exp from "node:constants"
+
 export const UNCATEGORIZED = '__uncategorized__'
 
 export const PAGE_SIZE = 8
@@ -9,11 +11,15 @@ export const DEFAULT_THEME = DARK_MODE
 
 // Banner height unit: vh
 export const BANNER_HEIGHT = 50
+export const BANNER_HEIGHT_MOBILE = 20 // Smaller height for mobile
 export const BANNER_HEIGHT_EXTEND = 30
+export const BANNER_HEIGHT_EXTEND_MOBILE = 10 // Smaller extend for mobile
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND
+export const BANNER_HEIGHT_HOME_MOBILE = BANNER_HEIGHT_MOBILE + BANNER_HEIGHT_EXTEND_MOBILE // smaller height for mobile
 
 // The height the main panel overlaps the banner, unit: rem
 export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5
+export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT_MOBILE = 2 // Smaller height for mobile
 
 // Page width: rem
 export const PAGE_WIDTH = 75
