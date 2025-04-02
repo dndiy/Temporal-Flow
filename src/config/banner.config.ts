1668aba80d052e013ce1fae6798d9810fde50afc
Greg Aster <greg@dndiy.org>
2025-04-01 23:39:43 -0700

    generic banner additions

--- src/config/banner.config.ts
+++ src/config/banner.config.ts
@@ -9,8 +9,8 @@ import banner3 from 'src/assets/banner/0003.png'
 import banner4 from 'src/assets/banner/0004.png'
 import banner5 from 'src/assets/banner/0005.png'
 import banner6 from 'src/assets/banner/0006.png'
-//import banner7 from 'src/assets/banner/0007.png' continue as long as you want
-//import banner8 from 'src/assets/banner/0004.png' continue as long as you want
+import banner7 from 'src/assets/banner/0007.png'
+import banner8 from 'src/assets/banner/0008.png'
 
 // Define the banner configuration type
 export interface BannerConfig {
@@ -71,8 +71,8 @@ export const bannerConfig: BannerConfig = {
     banner4,
     banner5,
     banner6,
-    //banner7, add more banners here
-    //banner8 continue as long as you want
+    banner7,
+    banner8
   ],
   
   // Default banner image (used for static banner or as first animation frame)
