31a1b44b2a5aa98d54f45df45e47ccff80d500b5
Greg Aster <greg@dndiy.org>
2025-04-02 15:05:05 -0700

    refactor: update deployment workflow and remove obsolete rebuild workflow

---
+++ src/config/config (copy).ts
@@ -0,0 +1,101 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from '../types/config'
+import { LinkPreset } from '../types/config'
+import { AUTO_MODE, DARK_MODE, LIGHT_MODE } from '@constants/constants.ts'
+
+
+export const siteConfig: SiteConfig = {
+  title: "Temporal Flow",
+  subtitle: "A Decentralized Content Platform",
+  lang: "en",
+  themeColor: {
+    hue: 0,
+    fixed: false
+  },
+  defaultTheme: LIGHT_MODE,
+  banner: {
+    enable: false,
+    src: "/assets/banner/0001.png",
+    position: "center",
+    credit: {
+      enable: true,
+      text: "",
+      url: ""
+    }
+  },
+  toc: {
+    enable: true,
+    depth: 3
+  },
+  favicon: []
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    0,
+    {
+      name: "Projects",
+      url: "/archive/",
+      dropdown: [
+        {
+          name: "Timeline Example",
+          url: "/posts/timeline/"
+        },
+        {
+          name: "Project One",
+          url: "/projects/project-one/"
+        },
+        {
+          name: "Project Two",
+          url: "/projects/project-two/"
+        },
+        {
+          name: "Project Three",
+          url: "/projects/project-three/"
+        },
+        {
+          name: "External Project",
+          url: "https://example.com/project",
+          external: true
+        }
+      ]
+    },
+    3,
+    1,
+    2
+  ]
+}
+
+export const profileConfig: ProfileConfig = {
+  avatar: "/src/content/avatar/avatar.png",
+  name: "Site Owner",
+  bio: "Content Creator & Digital Storyteller",
+  links: [
+    {
+      name: "Twitter",
+      icon: "fa6-brands:twitter",
+      url: "https://twitter.com/example"
+    },
+    {
+      name: "Discord",
+      icon: "fa6-brands:discord",
+      url: "https://discord.gg/example"
+    },
+    {
+      name: "GitHub",
+      icon: "fa6-brands:github",
+      url: "https://github.com/example"
+    }
+  ],
+  avatarFilename: "ComfyUI_0003.png"
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: true,
+  name: "CC BY-NC-SA 4.0",
+  url: "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+}
