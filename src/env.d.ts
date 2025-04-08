c27a6df5d813fa512ea00363b8da3fcbefdcbc29
Greg Aster <greg@dndiy.org>
2025-04-08 14:14:14 -0700

    flug file data screw up fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,1 @@
-/// <reference types="astro/client" />
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-
-interface Window {
-    [key: string]: any;
-  }
+types/aboutconfig.ts types/communityconfig.ts types/config.ts
