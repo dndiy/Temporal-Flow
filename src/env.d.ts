0adb6cb52b23cbc55ce467944004239dec9d8ee2
Greg Aster <greg@dndiy.org>
2025-04-08 22:23:30 -0700

    failed build - env.d.ts debug

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,7 @@
-types/aboutconfig.ts types/communityconfig.ts types/config.ts
+/// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface Window {
+    [key: string]: any;
+  }
